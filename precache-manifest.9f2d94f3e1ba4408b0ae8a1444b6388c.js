395d11df588cb753c6e1d6b9990e6a4211edf159
Calvin Laptop <calvindonaldson1997313@gmail.com>
2020-05-03 19:15:37 +0100

    Updates

---
+++ precache-manifest.9f2d94f3e1ba4408b0ae8a1444b6388c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d0cf301ae8825d3b6c2b8488e1185d",
+    "url": "/marvelvsdc/index.html"
+  },
+  {
+    "revision": "e1bc04c1e324996b7c72",
+    "url": "/marvelvsdc/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "5b2a9b039537f7bf9b03",
+    "url": "/marvelvsdc/static/css/main.bb9e5646.chunk.css"
+  },
+  {
+    "revision": "e1bc04c1e324996b7c72",
+    "url": "/marvelvsdc/static/js/2.22ea8ffd.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/marvelvsdc/static/js/2.22ea8ffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b2a9b039537f7bf9b03",
+    "url": "/marvelvsdc/static/js/main.13e0b65f.chunk.js"
+  },
+  {
+    "revision": "0e1df754096543d743c2",
+    "url": "/marvelvsdc/static/js/runtime-main.7b292817.js"
+  },
+  {
+    "revision": "28721de90db97a0afb7838d85a843817",
+    "url": "/marvelvsdc/static/media/marvel.28721de9.png"
+  }
+]);
